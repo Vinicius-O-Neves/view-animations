7f4023c2e49f51ba0457ea5044553489507c52da
Vinicius Neves <vinicius.neeves@outlook.com>
2022-06-23 12:31:23 -0300

    first commit

---
+++ ViewAnimations/src/main/java/app/dealux/viewanimationslibrary/ViewAnimations.kt
@@ -0,0 +1,65 @@
+package app.dealux.viewanimationslibrary
+
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import app.dealux.viewanimations.R
+
+fun View.fadeIn(duration: Long, endAction: () -> Unit = {}) = animate()
+    .also { clearAnimation() }
+    .setDuration(duration)
+    .alpha(1f)
+    .withStartAction {
+        visibility = View.INVISIBLE
+        alpha = 0f
+    }
+    .withEndAction {
+        visibility = View.VISIBLE
+        endAction()
+    }
+    .start()
+
+fun View.fadeOut(duration: Long = 0, endAction: () -> Unit = {}) = animate()
+    .also { clearAnimation() }
+    .setDuration(duration)
+    .alphaBy(-1f)
+    .withStartAction { alpha = 1f }
+    .withEndAction {
+        visibility = View.INVISIBLE
+        endAction()
+    }
+    .start()
+
+fun View.explode(endAction: () -> Unit = {}) {
+    val explodeAnim = AnimationUtils.loadAnimation(this.context, R.anim.explode)
+
+    this.visibility = View.VISIBLE
+    this.startAnimation(explodeAnim)
+
+    explodeAnim.apply {
+        alpha = 0f
+    }
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        endAction()
+    }, 500)
+}
+
+fun View.shake(view: View) {
+    val startX = 0f
+    val translationX = 35f
+    val duration = 100L
+
+    repeat(2) {
+        view.animate()
+            .translationX(translationX)
+            .setDuration(duration)
+            .withEndAction {
+                view.animate()
+                    .translationX(startX)
+                    .duration = duration
+            }
+            .start()
+    }
+}
