cbe2d4d2c84bfed170dc93db8d37576e8673c207
Vinicius Neves <vinicius.neeves@outlook.com>
2022-06-23 14:48:26 -0300

    updated version

--- app/src/main/java/app/dealux/viewanimations/MainActivity.kt
+++ app/src/main/java/app/dealux/viewanimations/MainActivity.kt
@@ -2,7 +2,6 @@ package app.dealux.viewanimations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
